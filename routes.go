61e66c880349540356d29761f8f09e0d7f3617eb
Chris Peck <onthelan@gmail.com>
2019-06-18 23:09:25 -0400

    Added article model template handlers

--- routes.go
+++ routes.go
@@ -6,4 +6,8 @@ func initializRoutes() {
 
 	// Handle the index router
 	router.GET("/", showIndexPage)
+
+	// Handle GET requests at /article/view/some_article_id
+	router.GET("/article/view/:article_id", getArticle)
+
 }
